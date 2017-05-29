a0563244eb28a7842300597b1e8b65021d14d243
bpali26 <bpali26@gmail.com>
2017-05-29 22:12:10 +0530

    Update Global.R

--- .gitignore/Global.R
+++ .gitignore/Global.R
@@ -7,4 +7,4 @@ library(tidytext)
 library(dplyr)
 
 #Setting up direct Authentication with twitter app
-setup_twitter_oauth(consumer_key= "lnGRtmneG8SpIjryTUO0OQOTx", consumer_secret = "CUTN9TEcS4R9yT7nOwRZwMcQfTR9tv6o1hDEs7u2ESALyCwJjQ",access_token = "1144248948-sYwoL3H1qRojjE2asTbc9uBbuE3Qz430PeZZoSE",access_secret = "xFOgj2tQuZ0FKAgAcjcIzzUvtu8EnzmhijhtGSN7nhMWb")
+setup_twitter_oauth(consumer_key= "ENTER YOUR CONSUMER KEY", consumer_secret = "ENTER YOUR CONSUMER SECRET",access_token = "ENTER YOUR ACCESS TOKEN",access_secret = "ENTER YOUR ACCESS SECRET")
